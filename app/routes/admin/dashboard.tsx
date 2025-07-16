e01342c2d8470aed5fa7911673e7511e3122009d
Marwan1998 <dragonkingofhell@gmail.com>
2025-07-16 23:37:46 +0200

    designing dashboard page

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,7 +1,9 @@
-import { Header } from "components"
+import { Header, StatsCard, TripCard } from "components"
+import { user, dashboardStats, allTrips } from "~/constants"
+
+const {totalUsers, usersJoined, totalTrips, tripsCreated, userRole} = dashboardStats
 
 const dashboard = () => {
-  const user = {name: 'Marwan'};
   return (
     <main className="dashboard wrapper">
       <Header
@@ -9,7 +11,50 @@ const dashboard = () => {
         description="Track activity, trends and popular destinations in real time"  
       />
 
-      dashboard content
+      <section className="flex flex-col gad-6">
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+          <StatsCard
+            headerTitle="Totla Users"
+            total={totalUsers}
+            currentMonthCount={usersJoined.currentMonth }
+            lastMonthCount={usersJoined.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Totla Trips"
+            total={totalTrips}
+            currentMonthCount={tripsCreated.currentMonth }
+            lastMonthCount={tripsCreated.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Active Users"
+            total={userRole.total}
+            currentMonthCount={userRole.currentMonth }
+            lastMonthCount={userRole.lastMonth}
+          />
+        </div>
+
+      </section>
+
+      <section className="container">
+        
+        <h1 className="text-xl font-semibold text-dark-100">Created Trips</h1>
+        <div className="trip-grid">
+          {allTrips.slice(0, 4).map((trip) => (
+            <TripCard
+              key={trip.id}
+              id={trip.id.toString()}
+              name={trip.name}
+              imageUrl={trip.imageUrls[0]}
+              location={trip.itinerary?.[0]?.location ?? ''}
+              tags={trip.tags}
+              price={trip.estimatedPrice}
+            /> 
+          ))}
+        </div>
+
+      </section>
+
       
     </main>
   )
